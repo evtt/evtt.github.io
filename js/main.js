f629d33b93f5e01ce075d6c43bf885c8db8edaae
evtt <68776569+evtt@users.noreply.github.com>
2020-08-01 11:58:54 +0800

    Add the calculator formula.

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function generate_number()
+{
+	number_01 = Math.floor(Math.random() * 100);
+	number_02 = Math.floor(Math.random() * 100);
+	arrary_number = [number_01, number_02];
+	document.getElementById("calculate").innerHTML = number_01 + " + " + number_02;
+	return arrary_number;
+}
+
+function check_answer()
+{
+	sum = Number(number_01) + Number(number_02);
+	answer = document.getElementById("answer").value;
+	question = document.getElementById("question").innerHTML;
+
+	if(answer != null
+		&& answer.toString().length
+		== sum.toString().length)
+	{
+		if(Number(sum) == Number(answer))
+		{
+			document.getElementById("question").innerHTML = (Number(question) + 1).toString();
+			document.getElementById("answer").value = "";
+			generate_number();
+		}
+		else
+		{
+			alert("Game Over");
+			location.reload();
+		}
+	}
+}
